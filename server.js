85c62d5278f968571fab00b45ff5b7ccafabb42d
Diego Rodriguez <diego.f.rodriguezh@gmail.com>
2019-04-05 10:55:48 +1100

    Implemented webpack.config.js webpack dev middleware and webpack hot reloading.

--- server.js
+++ server.js
@@ -1,16 +1,39 @@
 const port = 3000;
+const path = require("path");
 const express = require("express");
 const app = express();
-const webpack = require('webpack');
+const webpack = require("webpack");
+const middleware = require("webpack-dev-middleware");
+const hotMiddleware = require("webpack-hot-middleware");
+const config = require("./webpack.config.js");
 
+// Configuring webpack
+const compiler = webpack(config);
 
-app.use(express.json());
-
-app.use(express.static(__dirname + '/dist'));
-app.get('*', function response(req, res) {
-  res.sendFile(path.join(__dirname, 'dist/index.html'));
+// Configuring webpack dev middleware
+const webpackMiddleware = middleware(compiler, {
+  publicPath: config.output.publicPath,
+  contentBase: "src",
+  stats: {
+    colors: true,
+    hash: false,
+    timings: true,
+    chunks: false,
+    chunkModules: false,
+    modules: false
+  }
 });
 
-app.listen(port, ()=>{
+app.use(webpackMiddleware);
+
+// Configuring webpack hot reload middleware.
+app.use(hotMiddleware(compiler));
+
+app.use(express.static(__dirname + "/dist"));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "dist/index.html"));
+});
+
+app.listen(port, () => {
   console.log(`Listening on port ${port}`);
-})
+});
---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+'use strict';
+
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+  devtool: 'eval-source-map',
+  mode: 'development',
+  entry: [
+    'webpack-hot-middleware/client?reload=true',
+    path.join(__dirname, 'src/index.js')
+  ],
+  output: {
+    path: path.join(__dirname, '/dist/'),
+    filename: '[name].js',
+    publicPath: '/'
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify('development')
+    })
+  ]
+};
