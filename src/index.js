34fd38abcd0b70d74d330771e6296f9c93c1f40d
Diego Rodriguez <diego.f.rodriguezh@gmail.com>
2019-04-05 13:08:21 +1100

    Added json, style-loader, css-loader, and sass-loader to webpack config.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import _ from 'lodash';
+import styles from './stylesheets/styles.scss';
 
 function component() {
   let element = document.createElement('div');
--- webpack.config.js
+++ webpack.config.js
@@ -1,37 +1,49 @@
-'use strict';
+"use strict";
 
-var path = require('path');
-var webpack = require('webpack');
+var path = require("path");
+var webpack = require("webpack");
 
 module.exports = {
-  devtool: 'eval-source-map',
-  mode: 'development',
+  devtool: "eval-source-map",
+  mode: "development",
   entry: [
-    'webpack-hot-middleware/client?reload=true',
-    path.join(__dirname, 'src/index.js')
+    "webpack-hot-middleware/client?reload=true",
+    path.join(__dirname, "src/index.js")
   ],
   output: {
-    path: path.join(__dirname, '/dist/'),
-    filename: '[name].js',
-    publicPath: '/'
+    path: path.join(__dirname, "/dist/"),
+    filename: "[name].js",
+    publicPath: "/"
   },
   plugins: [
     new webpack.HotModuleReplacementPlugin(),
     new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify('development')
+      "process.env.NODE_ENV": JSON.stringify("development")
     })
-  ], 
+  ],
   module: {
-    rules: [{
-      test: /\.jsx?$/,
-      exclude: /node_modules/,
-      loader: 'babel-loader'
-    }, {
-      test: /\.json?$/,
-      loader: 'json'
-    }, {
-      test: /\.css$/,
-      loader: 'style!css?modules&localIdentName=[name]---[local]---[hash:base64:5]'
-    }]
+    rules: [
+      {
+        test: /\.js?$/,
+        exclude: /node_modules/,
+        loader: "babel-loader"
+      },
+      {
+        test: /\.json?$/,
+        loader: "json"
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          "style-loader", // creates style nodes from JS strings
+          "css-loader", // translates CSS into CommonJS
+          "sass-loader" // compiles Sass to CSS, using Node Sass by default
+        ]
+      }
+    ]
   }
 };
